b129fe562e90c6631ebf7d4a7fe388afd97468e3
DeoSx <deosxz123@gmail.com>
2021-01-05 12:33:42 +0600

    feat: add navigation on folders

--- client/src/constrants.js
+++ client/src/constrants.js
@@ -3,3 +3,6 @@ export const LOGOUT = 'LOGOUT'
 
 export const SET_FILES = 'SET_FILES'
 export const SET_CUR_DIR = 'SET_CUR_DIR'
+export const ADD_FILE = 'ADD_FILE'
+export const POPUP_STATE = 'POPUP_STATE'
+export const PUSH_TO_STACK = 'PUSH_TO_STACK'
